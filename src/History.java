03cf2a04765f20963e15e3cdcfe088ee4c13fab7
samme121 <38052407+samme121@users.noreply.github.com>
2018-04-09 16:25:15 -0400

    Update History.java

--- src/History.java
+++ src/History.java
@@ -1,11 +1,59 @@
 public class History {
+  
   private User user;
   private Ticket ticket;
   private int uID;
   private int tID;
 
+  public History(User user, Ticket ticket){
+    this.user = user;
+    this.ticket = ticket;
+    this.uID = user.getUserID();
+    this.tID = ticket.getTicketID();
+  }
+  
+  public History(int uID, int tID){
+    this.uID = uID;
+    this.tID = tID;
+  }
+  
+  public History(){
+    user = NULL;
+    ticket = NULL;
+    uID = 0;
+    tID = 0;
+  }
 
-
-
+  public User getUser(){
+   return user; 
+  }
+  
+  public void setUser(User user){
+    this.user = user; 
+  }
+  
+  public Ticket getTicket(){
+    return ticket; 
+  }
+  
+  public void setTicket(Ticket ticket){
+   this.ticket = ticket; 
+  }
+  
+  public int getUserID(){
+   return uID; 
+  }
+  
+  public void setUserID(int uID){
+   this.uID = uID; 
+  }
+  
+  public int getTicketID(){
+   return tID; 
+  }
+  
+  public void setTicketID(int tID){
+    this.tID = tID;
+  }
 
 }
